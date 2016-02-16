3e4fd61d8fa67fd6cac24410a72e3402dbe22e9c
FusixGit <nikita0204@mail.ru>
2016-02-16 19:14:48 +0300

    Update MemoryModule.h

--- MemoryModule.h
+++ MemoryModule.h
@@ -1,43 +1,24 @@
-/*
- * Memory DLL loading code
- * Version 0.0.4
- *
- * Copyright (c) 2004-2015 by Joachim Bauch / mail@joachim-bauch.de
- * http://www.joachim-bauch.de
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is MemoryModule.h
- *
- * The Initial Developer of the Original Code is Joachim Bauch.
- *
- * Portions created by Joachim Bauch are Copyright (C) 2004-2015
- * Joachim Bauch. All Rights Reserved.
- *
- */
-
 #ifndef __MEMORY_MODULE_HEADER
 #define __MEMORY_MODULE_HEADER
 
+#include "stdafx.h"
 #include <windows.h>
 
 typedef void *HMEMORYMODULE;
-
 typedef void *HMEMORYRSRC;
-
 typedef void *HCUSTOMMODULE;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct
+{
+	void* EntryPoint;
+	wchar_t* CommandLine;
+	bool InThread;
+	void* _ModuleHandle;
+}	MEMORYMODULE_START_ARGS;
 
 typedef HCUSTOMMODULE (*CustomLoadLibraryFunc)(LPCSTR, void *);
 typedef FARPROC (*CustomGetProcAddressFunc)(HCUSTOMMODULE, LPCSTR, void *);
@@ -85,7 +66,8 @@ void MemoryFreeLibrary(HMEMORYMODULE);
  *
  * Returns a negative value if the entry point could not be executed.
  */
-int MemoryCallEntryPoint(HMEMORYMODULE);
+int MemoryCallEntryPoint(HMEMORYMODULE, MEMORYMODULE_START_ARGS*);
+
 
 /**
  * Find the location of a resource with the specified type and name.
@@ -140,9 +122,10 @@ FARPROC MemoryDefaultGetProcAddress(HCUSTOMMODULE, LPCSTR, void *);
  * This is the default as used by MemoryLoadLibrary.
  */
 void MemoryDefaultFreeLibrary(HCUSTOMMODULE, void *);
+void MemoryModuleExecuteCode(MEMORYMODULE_START_ARGS*);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // __MEMORY_MODULE_HEADER
+#endif
